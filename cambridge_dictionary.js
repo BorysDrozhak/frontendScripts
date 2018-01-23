a01428cd1806bf1df5a3a8b018a4725c4092c0ce
bdrozhak <bdrozhak@cisco.com>
2018-01-23 17:52:50 +0200

    add close modal if open form by escape key: CD

--- cambridge_dictionary.js
+++ cambridge_dictionary.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Cambridge Dictionary Fast Navigation
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  papaparam
 // @author       Borys Drozhak
 // @include      /^https://dictionary.cambridge.org/
@@ -13,13 +13,13 @@
 
     document.onkeyup = function(event) {
         if (event.code == "Escape") {
+            var modal = document.getElementsByClassName("modal modal--myd js-modal open")[0]; // get modal if it is open
+            if (modal)
+                modal.querySelector('#modal-login>[data-target-selector]').click(); // click close
+            // move cursor to search box and select the text
             var inp = document.getElementById("cdo-search-input");
             inp.focus();
             inp.setSelectionRange(0, inp.value.length);
         }
-        // if (event.code == "Escape") {
-        //     var inp = document.getElementById("gt-swap").click();
-        // }
-        
     };
 })();
